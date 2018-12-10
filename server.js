9adaa37a7f861a542913734ae07f6c8d09ddb5f3
ZIKAI ZHAO <u5655826@anu.edu.au>
2018-12-10 13:58:20 +1100

    change username

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.post('/api/email',(req,res) => {
     nodemailer.createTestAccount(() => {
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false, // true for 465, false for other ports
+            host: 'rsl-wc2.au.syrahost.com',
+            port: 465,
+            secure: true, // true for 465, false for other ports
             auth: {
-                user: 'chinazzk94@gmail.com', 
-                pass: 'etmjmqgatkfciata'
+                user: 'proxy@paylinx.com.au', 
+                pass: 'he};1U)o5WVE'
             }
         });
 
